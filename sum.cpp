17a768e6e4612fbec06a78c9373fda172942ef00
MD ANWAR HOSSEN <157793125+anwarhossen57@users.noreply.github.com>
2025-01-05 15:16:32 +0600

    sum of two input variable

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+    int sum;
+    cout << " Type a number : ";
+    cin >> x;
+    cout << " Type a another number : ";
+    cin >> y;
+    sum = x + y ;
+    cout << " Total number : " <<sum <<endl;
+    return 0;
+}
