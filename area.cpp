4837d620d4ee60f616cd8dccfcf9858919afb3bb
MD ANWAR HOSSEN <157793125+anwarhossen57@users.noreply.github.com>
2025-01-05 14:37:37 +0600

    Update area.cpp

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length = 20;
+    int width = 23;
+    int area;
+    area = length * width;
+
+    cout << "Area of Rectangle : " <<area<<endl;
+    return 0;
+}
