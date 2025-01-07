5615bd1e351091bb8b1a087f1c5df6f68851ebfc
MD ANWAR HOSSEN <157793125+anwarhossen57@users.noreply.github.com>
2025-01-07 10:54:14 +0600

    Check day

---
+++ switch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day = 5;
+    switch (day)
+    {
+    case 1:
+        cout << "saturday";
+        break;
+    case 2:
+        cout << "Sunday";
+        break;
+    case 3:
+        cout <<"Monday";
+        break;
+    case 4:
+        cout <<"Tuesday";
+        break;
+    case 5:
+        cout <<"Wednesday";
+        break;
+    case 6:
+        cout <<"Thursday";
+        break;
+    case 7:
+        cout <<"Friday";
+        break;
+    }
+    return 0;
+}
