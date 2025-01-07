a0c0f5bca7507b8fcb29f88ae8272d0ba5187846
MD ANWAR HOSSEN <157793125+anwarhossen57@users.noreply.github.com>
2025-01-07 22:32:31 +0600

    Student information

---
+++ stinfo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int studentId = 10;
+    int studentAge = 23;
+    float studentFee = 78.34; 
+    char studentGrade = 'A';
+
+    cout << "Student Id : "<<studentId<<"\n";
+    cout << "Student Age : "<<studentAge<<"\n";
+    cout << "Student Fee : "<<studentFee<<"\n";
+    cout << "Student Grade : "<<studentGrade<<"\n";
+
+    return 0;
+
+}
